580dd67b209c4e7b977924989664bc0a80d9f6fa
Ben Ethington <benjaminde@gmail.com>
2023-08-28 00:58:38 -0500

    aider: Implemented the `scrapeBookFromGutenberg` function in `src\utils\gutenberg_scraper.ts` to scrape a book from Gutenberg and convert it to markdown.
    # Aider chat conversation:
    
    USER: file `src\utils\gutenberg_scraper.ts` has been created add proposed changes to it.
    
    ASSISTANT: Great! Now we can implement the `scrapeBookFromGutenberg` function in the `src\utils\gutenberg_scraper.ts` file. This function will use a web scraping library like `axios` and `cheerio` to scrape the book from Gutenberg and convert it to markdown.
    
    Here is the proposed change:
    
    ```typescript
    src\utils\gutenberg_scraper.ts
    <<<<<<< HEAD
    
    =======
    import axios from 'axios';
    import cheerio from 'cheerio';
    
    export async function scrapeBookFromGutenberg(bookTitle: string): Promise<string> {
      // Replace spaces with '+' for the URL
      const formattedTitle = bookTitle.replace(/ /g, '+');
    
      // Get the book page
      const response = await axios.get(`https://www.gutenberg.org/ebooks/search/?query=${formattedTitle}`);
      const $ = cheerio.load(response.data);
    
      // Get the book URL
      const bookUrl = $('li.booklink a').attr('href');
    
      // Get the book text
      const bookResponse = await axios.get(`https://www.gutenberg.org${bookUrl}`);
      const book$ = cheerio.load(bookResponse.data);
    
      // Convert the book text to markdown
      const bookMarkdown = book$('body').text();
    
      return bookMarkdown;
    }
    >>>>>>> updated
    ```
    
    Please note that this is a basic implementation and might not work for all books on Gutenberg. You might need to adjust the selectors depending on the structure of the book pages. Also, this function does not handle errors. You might want to add error handling to improve its robustness.

---
+++ src/utils/gutenberg_scraper.ts
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import cheerio from 'cheerio';
+
+export async function scrapeBookFromGutenberg(bookTitle: string): Promise<string> {
+  // Replace spaces with '+' for the URL
+  const formattedTitle = bookTitle.replace(/ /g, '+');
+
+  // Get the book page
+  const response = await axios.get(`https://www.gutenberg.org/ebooks/search/?query=${formattedTitle}`);
+  const $ = cheerio.load(response.data);
+
+  // Get the book URL
+  const bookUrl = $('li.booklink a').attr('href');
+
+  // Get the book text
+  const bookResponse = await axios.get(`https://www.gutenberg.org${bookUrl}`);
+  const book$ = cheerio.load(bookResponse.data);
+
+  // Convert the book text to markdown
+  const bookMarkdown = book$('body').text();
+
+  return bookMarkdown;
+}
